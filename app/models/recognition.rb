d52627c4a4332d101525e7d0e93e69d07ef92011
Larissa Pissurno <larissapissurno@gmail.com>
2025-10-15 15:14:17 -0300

    move list to a partial

--- app/models/recognition.rb
+++ app/models/recognition.rb
@@ -4,4 +4,11 @@ class Recognition < ApplicationRecord
   belongs_to :badge
 
   validates :message, presence: true
+
+  after_create_commit -> {
+    broadcast_replace_to "recognitions",
+      target: "recent_recognitions",
+      partial: "recognitions/list",
+      locals: { recognitions: Recognition.all.order(created_at: :desc) }
+  }
 end
